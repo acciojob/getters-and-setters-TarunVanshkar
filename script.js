e68553a604d37ff5010e1e283177c34f02dc6804
TarunVanshkar <117818351+TarunVanshkar@users.noreply.github.com>
2023-05-15 23:34:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 //complete this code
-class Person {}
+class Person {
+	#name;
+    #age;
+    constructor(name, age){
+        this.#name = name;
+        this.#age = age;
+    }
+    get getName(){
+        return this.#name;
+    }
+    set setAge(age){
+        this.#age = age;
+    }
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+        console.log(this.getName() +" is studying");
+    }
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+        console.log(this.getName() +" is teaching");
+    }
+}
 
 // Do not change the code below this line
 window.Person = Person;
