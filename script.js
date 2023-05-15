eb2b793846462e9d3c5d87d31a0c3ca2c927a590
TarunVanshkar <117818351+TarunVanshkar@users.noreply.github.com>
2023-05-15 23:39:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ class Person {
 
 class Student extends Person {
 	study(){
-        console.log(this.getName +" is studying");
+        console.log(`${this.name} is studying`);
     }
 }
 
 class Teacher extends Person {
 	teach(){
-        console.log(this.getName +" is teaching");
+        console.log(`${this.name} is studying teaching`);
     }
 }
 
